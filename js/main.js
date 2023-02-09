41d132e7b0d855be137d2b2f442e377ba666c684
Dmitry <fogosony700@gmail.com>
2023-02-09 10:12:37 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+AOS.init({
+  disable: "phone",
+});
+
+const menu = document.querySelector(".header__list");
+const burger = document.querySelector(".header__burger");
+
+burger.addEventListener("click", function() {
+  menu.classList.toggle("active");
+  burger.classList.toggle("active");
+  
+});
+
